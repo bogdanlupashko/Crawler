34ab8dd73c589c4a28d6544df82c75a84cb1d619
bogdanlupashko <bogdanlupashko@gmail.com>
2015-04-24 10:53:47 +0300

    refactoring

--- src/main/java/org/bl/crawler/ReportGenerator.java
+++ src/main/java/org/bl/crawler/ReportGenerator.java
@@ -8,7 +8,6 @@ import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.Writer;
 import java.io.StringWriter;
 import java.io.IOException;
@@ -20,83 +19,64 @@ import java.util.ArrayList;
  */
 public class ReportGenerator {
     private final String TEMPLATE = "HtmlReportTemplate.vm";
+    private final String HTML_EXTENSION = ".html";
+    private final String CSV_EXTENSION = ".csv";
     private static final String DESTINATION_FILE_NAME = "CrawlerReport";
+    private Writer writer = new StringWriter();
 
     public void getReport(String country, int countSites, boolean isCsv) {
+        ArrayList<Site> crawler = new AlexaCrawler(countSites).getInfo(country);
+
+        String content = null;
+        String extension = null;
+
         if (isCsv) {
-            createCsvReport(country, countSites);
+            content = fillCsvReport(country, crawler);
+            extension = CSV_EXTENSION;
         } else {
-            createHtmlReport(country, countSites);
+            content = fillHtmlReport(country, crawler);
+            extension = HTML_EXTENSION;
         }
+
+        writeFile(country, content, extension);
     }
 
-    private void createCsvReport(String country, int countSites) {
-        ArrayList<Site> list = runCrawler(country, countSites);
-        String csvHeader = "This page contains info about Top " + countSites + " sites in " + country;
-
-        try {
-            FileWriter writer = createCsvFile(country);
-
-            writer.append(csvHeader);
-            writer.append('\n');
-            writer.append("Country Rank");
-            writer.append(',');
-            writer.append("Site URL");
-            writer.append(',');
-            writer.append("Global Rank");
-            writer.append(',');
-            writer.append("Link to Alexa's page with site stats");
-            writer.append('\n');
-
-            for (int i = 0; i < list.size(); i++) {
-                writer.append(String.valueOf(list.get(i).getCurrentRank()));
-                writer.append(',');
-                writer.append(String.valueOf(list.get(i).getUrl()));
-                writer.append(',');
-                writer.append(String.valueOf(list.get(i).getGlobalRank()));
-                writer.append(',');
-                writer.append(String.valueOf(list.get(i).getAlexaUrl()));
-                writer.append('\n');
-            }
-            writer.flush();
-            writer.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private FileWriter createCsvFile(String country) throws Exception {
-        return new FileWriter(DESTINATION_FILE_NAME + country + ".csv");
-    }
-
-    private void createHtmlReport(String country, int countSites) {
-
+    private String fillHtmlReport(String country, ArrayList<Site> list) {
         VelocityEngine velocityEngine = new VelocityEngine();
         velocityEngine.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
         velocityEngine.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
         velocityEngine.init();
 
-        ArrayList list = runCrawler(country, countSites);
-
         VelocityContext velocityContext = new VelocityContext();
         velocityContext.put("sitesList", list);
         velocityContext.put("country", country);
-        velocityContext.put("countSites", countSites);
+        velocityContext.put("countSites", list.size());
 
         Template template = velocityEngine.getTemplate(TEMPLATE);
 
-        Writer writer = new StringWriter();
         template.merge(velocityContext, writer);
 
-        createHtmlFile(country, writer.toString());
+        return writer.toString();
     }
 
-    private ArrayList<Site> runCrawler(String country, int countSites) {
-        return new AlexaCrawler(countSites).getInfo(country);
+    private String fillCsvReport(String country, ArrayList<Site> list) {
+        StringBuilder context = new StringBuilder("This page contains info about Top " + list.size() + " sites in " + country + "\n"
+                + "Country Rank" + ","
+                + "Site URL" + ","
+                + "Global Rank" + ","
+                + "Link to Alexa's page with site stats" + "\n");
+
+        for (Site item : list) {
+            context.append(item.getCurrentRank()).append(",").
+                    append(item.getUrl()).append(",").
+                    append(item.getGlobalRank()).append(",").
+                    append(item.getAlexaUrl()).append("\n");
+        }
+        return context.toString();
     }
 
-    private void createHtmlFile(String country, String content) {
-        File file = new File(DESTINATION_FILE_NAME + country + ".html");
+    private void writeFile(String country, String content, String extension) {
+        File file = new File(DESTINATION_FILE_NAME + country + extension);
         try {
             FileUtils.write(file, content);
         } catch (IOException e) {
